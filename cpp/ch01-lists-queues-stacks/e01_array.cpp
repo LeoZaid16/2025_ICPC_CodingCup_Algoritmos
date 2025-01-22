989792763db595b06f5ae672d4ca8864a3a0f3a1
Leo <leonardo-zaid@outlook.es>
2025-01-22 16:29:02 -0600

    Arrays in c and java

--- cpp/ch01-lists-queues-stacks/e01_array.cpp
+++ cpp/ch01-lists-queues-stacks/e01_array.cpp
@@ -12,27 +12,28 @@
     // librerias
 #include <iostream>
 #include <array>
+using namespace std;
 
 int main()
 {
-    std::array<int, 10> arr;
+    array<int, 10> arr;
 
     arr[0] = 1;
-    std::cout << "First element:" << arr[0] << std::endl;
+    cout << "First element:" << arr[0] << endl;
 
-    std::array<int, 4> arr2 = {1, 2, 3, 4};
-    std::cout << "Elements in second array: ";
+    array<int, 4> arr2 = {1, 2, 3, 4};
+    cout << "Elements in second array: ";
     for(int i = 0; i < arr.size(); i++)
-        std::cout << arr2[i] << " ";
+        cout << arr2[i] << " ";
 
     // exception
     try
     {
-        std::cout << arr2.at(4);
-        std::cout << arr2.at(5);
+        cout << arr2.at(4);
+        cout << arr2.at(5);
     }
     catch (const out_of_range& ex)
     {
-        std::cerr << "EXCEPTION: " << ex.what();
+        cerr << "EXCEPTION: " << ex.what();
     }
 }
