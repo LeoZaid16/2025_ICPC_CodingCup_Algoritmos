c3215fcf99f24dc1c3df7f25f0e8fd5317c853b7
Leo <leonardo-zaid@outlook.es>
2025-01-21 21:59:39 -0600

    ICPC initial repository local

---
+++ cpp/Test.cpp
@@ -0,0 +1,18 @@
+    /*
+    ITESS_TICS
+    ICPC
+    compilacion java y ejecución
+    21/02/2025
+    leonardo-zaid@outlook.es
+    */
+    //c++ Test.cpp -o Test.exe
+    //Test.exe
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello, programmers!!!" << std::endl;
+}
---
+++ java/Test.java
@@ -0,0 +1,15 @@
+    /*
+    ITESS_TICS
+    ICPC
+    compilacion java y ejecución
+    21/02/2025
+    leonardo-zaid@outlook.es
+    javac Test.java
+    java Test
+    */
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Hello, programmers!!!");
+    }
+}
